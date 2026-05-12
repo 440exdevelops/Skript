a01256d2d3d77f13d84045817420d8592c04e778
440exdevelops <jamesthestuff69420@gmail.com>
2026-05-11 22:42:41 -0400

    Create Locations.lua

---
+++ scripts/Locations.lua
@@ -0,0 +1,66 @@
+local Locations = {
+    Eggs = {
+        
+    },
+    Worlds = {
+            ['The Overworld'] = Vector3.new(-0.373160, 9.196313, -21.460686),
+            ['Minigame Paradise'] = Vector3.new(9981.626953, 26.649769, 172.103104),
+            ['Seven Seas'] = Vector3.new(-23662.912109, 7.879493, -79.410988),
+			['Shadow Realm'] = Vector3.new(2418.608887, 3158.479004, 833.090454),
+            ['Summer World'] = Vector3.new(-371.416931, 13.657583, -4906.218262)
+    },
+    WorldIslands = {
+            Overworld = {
+                ['Home'] = Vector3.new(-0.373160, 9.196313, -21.460686),
+                ['Floating Island'] = Vector3.new(-15.394822, 423.056671, 143.418152),
+                ['Outer Space'] = Vector3.new(41.961628, 2663.166260, -6.398529),
+                ['Twilight'] = Vector3.new(-77.476852, 6862.482422, 88.328156),
+                ['The Void'] = Vector3.new(16.435265, 10145.999023, 151.715363),
+                ['Zen'] = Vector3.new(36.756794, 15971.724609, 41.872101)
+        },
+        MinigameParadise = {
+                ['Home'] = Vector3.new(9981.626953, 26.649769, 172.103104),
+                ['Dice Island'] = Vector3.new(9900.469727, 2907.709473, 208.326675),
+                ['Minecart Forest'] = Vector3.new(9882.379883, 7682.141602, 203.567032),
+                ['Robot Factory'] = Vector3.new(9887.796875, 13409.548828, 227.157822),
+                ['Hyperwave Island'] = Vector3.new(9885.633789, 20089.076172, 226.086578)
+        },
+        SevenSeas = {
+                ['Home'] = Vector3.new(-23662.912109, 7.879493, -79.410988),
+                ['Blizzard Hills'] = Vector3.new(-21425.126953, 6.632851, -100922.281250),
+                ['Poison Jungle'] = Vector3.new(-19331.593750, 6.675426, 18763.000000),
+                ['Infernite Volcano'] = Vector3.new(-17252.765625, 10.416322, -20406.792969),
+                ['Lost Atlantis'] = Vector3.new(-13946.125000, 8.150003, -20431.617188),
+                ['Dream Island'] = Vector3.new(-21817.914062, 7.596196, -20523.988281),
+                ['Classic Island'] = Vector3.new(-41525.742188, 8.381426, -20508.710938)
+        }
+    },
+    ItemShops = {
+		['Merchant thing'] = ,
+        ['Shadow Shop'] = ,
+        ['Alien Shop'] = ,
+        ['Blackmarket'] = ,
+        ['Dice Merchant'] = ,
+        ['Seven Seas Shop'] = ,
+        ['Red Dice Shop'] = 
+    },
+    Minigames = {
+        ['Board Game'] = ,
+        ['Pet Match'] = ,
+        ['Cart Escape'] = ,
+        ['Robot Claw'] = ,
+        ['Hyper Darts'] = ,
+        ['Doggy Jump'] = 
+    },
+    SellLocations = {
+        ['Overworld'] = 
+        ['Twilight'] = '',
+        ['Minigame Paradise'] = '',
+        ['Robot Factory'] = ''
+    },
+	Wheelspins = {
+		['Floating Island Wheelspin'] = Vector3.new(-19.214790, 425.601105, 172.936478)
+	}
+}
+
+return Locations
